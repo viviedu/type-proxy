9f74d699c8eee927969c34c77c41987bee580c0e
Tristan Penman <tristan@tristanpenman.com>
2024-11-18 14:36:54 +1100

    Add falseP and trueP

--- src/primitive.ts
+++ src/primitive.ts
@@ -7,6 +7,12 @@ export const booleanP: TypeProxy<boolean> = (value) => {
     : { success: false, error: ParseError.simpleError(value, 'a boolean') };
 };
 
+export const falseP: TypeProxy<false> = (value) => {
+  return value === false
+    ? { success: true, value: false }
+    : { success: false, error: ParseError.simpleError(value, 'false') };
+};
+
 export const nullP: TypeProxy<null> = (value) => {
   return value === null
     ? { success: true, value }
@@ -35,6 +41,12 @@ export const strLiteralP = <K extends string>(literal: K): TypeProxy<K> => {
   return (value) => value === literal
     ? { success: true, value: value as K }
     : { success: false, error: ParseError.simpleError(value, `"${literal}"`) };
+};
+
+export const trueP: TypeProxy<true> = (value) => {
+  return value === true
+    ? { success: true, value: true }
+    : { success: false, error: ParseError.simpleError(value, 'true') };
 };
 
 export const undefinedP: TypeProxy<undefined> = (value) => {
